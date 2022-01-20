9639cc17d87dc9a7c6aca9b2ee0b91df13674559
cjwheatley88 <cjwheatley88@gmail.com>
2022-01-20 18:35:59 +1100

    first draft

---
+++ linear-regression-main.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(dplyr)
+
+#linear regression flow:
+#theory on correlation and hypothesis 
+#visualise correlation and overlay hypothesis
+#theory on error
+#visualise error
+#theory on error reduction through interation or math
+#demonstrate with code
+#demonstrate with preexisting vectorised tools
+
+#source data.. Titanic?
